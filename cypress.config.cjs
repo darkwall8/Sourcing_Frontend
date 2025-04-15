fb650a4dc3a3eb870b86b5c46413455bdbafaf5b
FOKO KENMOGNE Wilfried <fokowilfried7@gmail.com>
2025-04-15 19:53:29 +0100

    update: add the e2e test template and the cypress github action

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,11 +1,19 @@
 // cypress.config.cjs
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'e7dqif',
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
       // ...
-    }
-  }
-})
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+});
--- src/components/DashBoardHeader.tsx
+++ src/components/DashBoardHeader.tsx
@@ -1,5 +1,4 @@
 import { useTranslation } from "react-i18next";
-import DarkModeToggle from "./DarkModeToggle";
 import LanguageSelector from "./LanguageSelector";
 import SearchBar from "./ui/SearchBar";
 
@@ -11,7 +10,6 @@ function DashBoardHeader() {
         <div className="w-full h-24 border-b flex !px-32 border-gray-300 items-center justify-between">
             <p>{ t("greeting") } Wilfried</p>
             <SearchBar />
-            <DarkModeToggle />
             <LanguageSelector />
         </div>
     )
