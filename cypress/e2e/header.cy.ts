1f4db07998597a64a8c1af190efcfa5c01b4306b
FOKO KENMOGNE Wilfried <fokowilfried7@gmail.com>
2025-04-18 15:24:29 +0100

    update: the login test is ok !

--- cypress/e2e/header.cy.ts
+++ cypress/e2e/header.cy.ts
@@ -4,16 +4,16 @@ describe.skip('header', () => {
       if (response.status === 200) {
         cy.visit('/')
 
-        cy.contains("Connectez vous").click()
+        cy.contains("Connectez vous").click();
         // Should be on a new URL whitch include /login
-        cy.url().should("include", "/login")
+        cy.url().should("include", "/login");
 
-        cy.contains("SOURCING").click()
-        cy.url().should("eq", Cypress.config().baseUrl + "/")
+        cy.contains("SOURCING").click();
+        cy.url().should("eq", Cypress.config().baseUrl + "/");
 
-        cy.contains("Rejoignez nous!").click()
+        cy.contains("Rejoignez nous!").click();
         // Should be on a new URL whitch include /registration
-        cy.url().should("include", "/registration")
+        cy.url().should("include", "/registration");
 
       } else {
         cy.log("Page /login non disponible, test ignoré")
--- cypress/e2e/showCasesPages/login.cy.ts
+++ cypress/e2e/showCasesPages/login.cy.ts
@@ -1,10 +1,30 @@
+import API from "../../../src/utils/API";
+const api = new API();
+
 describe('login page', () => {
   it('test login page only if exists', () => {
     cy.request({ url: '/login', failOnStatusCode: false }).then((response) => {
       if (response.status === 200) {
-        it('passes', () => {
-          cy.visit('/login')
-        })
+
+        cy.intercept("POST", `${api.apiUrl}/api/auth/login`).as("loginRequest");
+
+          cy.visit('/login');
+
+          cy.get("input[type='email']").type("fokowilfried7@gmail.com");
+          cy.get("input[type='password']").type("1234");
+
+          cy.contains("Se connecter").click();
+
+          // Intercept the request
+          cy.wait("@loginRequest").then((intercept) => {
+            expect(intercept.request.body).to.deep.equal({
+              email: "fokowilfried7@gmail.com",
+              password: "1234"
+            });
+
+            expect(intercept.response?.statusCode).to.eq(200);
+          })
+
       } else {
         cy.log("Page non disponible, test ignoré")
       }
