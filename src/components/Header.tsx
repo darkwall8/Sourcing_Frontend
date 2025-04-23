1eb539debd47524f8cfa0d25c76ec202cfbb5666
the-pek <pierre.kengne@2027.ucac-icam.com>
2025-04-23 12:49:06 +0100

    feature-header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,32 @@
 import { useTranslation } from "react-i18next";
+import LogoSourcing from "/logo/sourcing_logo.svg"
+// import LanguageSelector from "./LanguageSelector";
 
 function Header(){
 
     const { t } = useTranslation();
 
     return (
-        <div className="">Header</div>
+        
+        <div className="flex flex-row items-center justify-between mt-4 m-5">
+            <div className='flex items-center'>
+                <div className='w-20'>
+                    <img src={LogoSourcing} alt="sourcing_logo"/> 
+                </div>
+                <p className="text-primary text-2xl">
+                    Sourcing
+                </p>
+            </div>
+            <div className="flex gap-1.5">
+                <button className="text-primary border rounded-md bg-white h-9 w-36 hover:cursor-pointer hover:bg-primary hover:text-white">
+                    {t("connect")}
+                </button>
+                <button className="text-white border rounded-md bg-primary h-9 w-36 hover:cursor-pointer hover:bg-white hover:text-primary">
+                    {t("rejoin")}
+                </button>
+            </div>
+        </div>
+        
     )
 }
 
