f19b7625b7b852a7cb932bf7c77fa189d83db5b0
the-pek <pierre.kengne@2027.ucac-icam.com>
2025-04-24 09:33:16 +0100

    feature-header

--- cypress/e2e/header.cy.ts
+++ cypress/e2e/header.cy.ts
@@ -1,30 +1,18 @@
-describe.skip('header component', () => {
+describe('header component', () => {
   it('test header only if exists', () => {
 
     cy.visit('/')
 
-    let shouldTest = false;
+    cy.contains("Connectez vous").click();
+    // Should be on a new URL whitch include /login
+    cy.url().should("include", "/login");
 
-    cy.get('body').then(($body) => {
-      if ($body.find('[data-canbetest="true"]').length > 0) {
-        shouldTest = true;
-      }
-    }).then(() => {
-      if (shouldTest) {
+    cy.contains("Sourcing").click();
+    cy.url().should("eq", Cypress.config().baseUrl + "/");
 
-        cy.contains("Connectez vous").click();
-        // Should be on a new URL whitch include /login
-        cy.url().should("include", "/login");
+    cy.contains("Rejoignez nous!").click();
+    // Should be on a new URL whitch include /registration
+    cy.url().should("include", "/registration");
 
-        cy.contains("SOURCING").click();
-        cy.url().should("eq", Cypress.config().baseUrl + "/");
-
-        cy.contains("Rejoignez nous!").click();
-        // Should be on a new URL whitch include /registration
-        cy.url().should("include", "/registration");
-      } else {
-        cy.log("Page non disponible, test ignoré");
-      }
-    });
   });
 });
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,53 @@
 import { useTranslation } from "react-i18next";
 import LogoSourcing from "/logo/sourcing_logo.svg"
-// import LanguageSelector from "./LanguageSelector";
+import { useNavigate } from "react-router-dom";
 
 function Header(){
 
     const { t } = useTranslation();
+    const navigate= useNavigate();
+    let path
+
+    function redirectHome() {
+        path= "/"
+        navigate(path)
+    }
+
+    function redirectLogin(){
+        path= "/login"
+        navigate(path)
+    }
+
+    function redirectRegistration(){
+        path="/registration"
+        navigate(path)
+    }
 
     return (
-        
-        <div className="flex flex-row items-center justify-between mt-4 m-5">
-            <div className='flex items-center'>
-                <div className='w-20'>
+        <div data-canbetest="true" className=" flex items-center justify-between mt-4 m-5 hover:cursor-pointer max-sm:flex-col max-sm:gap-3">
+            <div className='flex items-center' onClick={redirectHome}>
+                <div className='w-20 max-phone:w-15'>
                     <img src={LogoSourcing} alt="sourcing_logo"/> 
                 </div>
-                <p className="text-primary text-2xl">
+                <p className="text-primary text-2xl max-phone:text-xl">
                     Sourcing
                 </p>
             </div>
-            <div className="flex gap-1.5">
-                <button className="text-primary border rounded-md bg-white h-9 w-36 hover:cursor-pointer hover:bg-primary hover:text-white">
+            <div className="flex gap-1.5 max-sm:flex-col">
+                <button 
+                    className="text-primary border rounded-lg bg-white hover:cursor-pointer pl-5 pr-5 p-2 shadow-lg max-phone:text-sm"
+                    onClick={redirectLogin}
+                >
                     {t("connect")}
                 </button>
-                <button className="text-white border rounded-md bg-primary h-9 w-36 hover:cursor-pointer hover:bg-white hover:text-primary">
+                <button
+                    className="text-white border rounded-lg bg-primary hover:cursor-pointer pl-5 pr-5 p-2 shadow-lg max-phone:text-sm"
+                    onClick={redirectRegistration}
+                >
                     {t("rejoin")}
                 </button>
             </div>
-        </div>
-        
+        </div> 
     )
 }
 
