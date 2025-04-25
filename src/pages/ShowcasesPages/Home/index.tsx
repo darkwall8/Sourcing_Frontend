72634f33c01516416ee739302ad19732ae432331
the-pek <pierre.kengne@2027.ucac-icam.com>
2025-04-25 14:16:58 +0100

    First overview of the home page

--- src/pages/ShowcasesPages/Home/index.tsx
+++ src/pages/ShowcasesPages/Home/index.tsx
@@ -1,30 +1,15 @@
-import EnterpriseSection from "./Section/EnterpriseSection";
-import StudentSection from "./Section/StudentSection";
-import { PageScroll } from "./Hooks/PageScroll.ts";
-import PageIndicator from "./components/PageIndicator";
-import { MediaQuery } from "./Hooks/MediaQuery.ts"
-import { useState, useEffect } from 'react'; // Ajout
+import { useTranslation } from "react-i18next";
+import  Check  from "/icons/check.svg";
+import HomeCompany from "/images/home_company_image.svg";
+import HomeStudent from "/images/home_student_image.svg";
+import IconCompany from "/icons/company.svg";
+import IconHappy from "/icons/happy.svg";
+import IconVector from "/icons/Vector-2.svg";
+import IconStudent from "/icons/students.svg";
+import IconSchool from "/icons/school.svg";
 
-function Home() {
-    const LargeScreen = MediaQuery('(min-width: 1024px)');
-    const { currentPage, scrollToPage } = PageScroll(LargeScreen)
-    const [visitedPages, setVisitedPages] = useState<Set<number>>(new Set([0])); // Nouvel état
-
-    useEffect(() => {
-        if (!LargeScreen) {
-            window.scrollTo(0, 0); // Force le scroll visuel vers le haut
-        }
-    }, [LargeScreen]);
-
-
-    useEffect(() => {
-        setVisitedPages(prev => {
-            if (prev.has(currentPage)) return prev;
-            const newSet = new Set(prev);
-            newSet.add(currentPage);
-            return newSet;
-        });
-    }, [currentPage]);
+function Home(){
+    const { t } = useTranslation();
 
     return (
         LargeScreen ? (
