12aed965d668903f26b2c3115f24eda08b1243f7
FOKO KENMOGNE Wilfried <fokowilfried7@gmail.com>
2025-05-28 10:04:51 +0100

    update: start the oimplementation of the step 2 of the student register stepper

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -1,6 +1,6 @@
-function Tag() {
+function Tag( { label } : { label: string } ) {
     return (
-        <div>Tag</div>
+        <div className="bg-primary py-1 px-4 rounded-md text-white text-sm w-fit">{ label }</div>
     )
 }
 
--- src/pages/ShowcasesPages/Registration/StudentRegistration/StudentRegistrationStep2.tsx
+++ src/pages/ShowcasesPages/Registration/StudentRegistration/StudentRegistrationStep2.tsx
@@ -5,16 +5,24 @@ import { useTranslation } from "react-i18next";
 import InputDropdown from "../../../../components/ui/InputDropdown";
 import Button from "../../../../components/ui/Button";
 import { useStudentInscription } from "../../../../utils/Context/StudentInscriptionContext";
+import Tag from "../../../../components/ui/Tag";
 
 function StudentRegistrationStep2( { handleSubmit, stepIndex } : { handleSubmit: (stepIndex: number) => void, stepIndex: number } ) {
 
     const { t } = useTranslation();
     const { studentSpecification, studentOfferPreferences, isStudentWantToReceiveNotification, setStudentSpecification, setStudentOfferPreferences, setIsStudentWantToReceiveNotification } = useStudentInscription();
+    const [value, setValue] = useState('');
+    const [items, setItems] = useState([]);
+
+    const search = (event) => {
+        // setItems([...Array(10).keys()].map(item => event.query + '-' + item));
+        console.log("...");
+    }
 
 
     const [isValidatedValues, setIsValidatedValues] = useState(false);
     const isValid = () => {
-        return !!studentSpecification.trim() && !!studentOfferPreferences;
+        return !!studentSpecification.trim() && studentOfferPreferences.length > 0;
     };
     const specificationList = [
         {
@@ -41,22 +49,25 @@ function StudentRegistrationStep2( { handleSubmit, stepIndex } : { handleSubmit:
                         <InputDropdown label={t("your_specification")} placeholder={t("specification_placeholder")} isRequired={true} value={studentSpecification} handleChange={setStudentSpecification} showValidationErrors={isValidatedValues} options={specificationList  } />
                         <div>
                             <p>{ t("your_offer_preferences") }</p>
-
+                            {/*  */}
+                            <div className="flex flex-wrap gap-2">
+                                <Tag label={t("tag")} />
+                            </div>
                         </div>
                     </div>
                     <div className="py-8">
                         <p>{ t("do_you_want_to_receive_notification") }</p>
                         <div className="flex gap-8">
-                            <div onClick={() => setIsStudentWantToReceiveNotification(true)} className="flex gap-2 justify-center items-center">
+                            <div onClick={() => setIsStudentWantToReceiveNotification(true)} className="flex gap-2 justify-center items-center cursor-pointer">
                                 <p>{ t("yes") }</p>
-                                <div className={`${isStudentWantToReceiveNotification ? "bg-primary" : "bg-white border-2"} w-7 h-7 flex items-center justify-center rounded-md transition-all duration-150 ease-in-out`}>
+                                <div className={`${isStudentWantToReceiveNotification ? "bg-primary" : "bg-white border-2"} w-6 h-6 flex items-center justify-center rounded-md transition-all duration-150 ease-in-out`}>
                                     <img src={check} alt="check" />
                                 </div>
                             </div>
                             {/*  */}
-                            <div onClick={() => setIsStudentWantToReceiveNotification(false)} className="flex gap-2 justify-center items-center">
+                            <div onClick={() => setIsStudentWantToReceiveNotification(false)} className="flex gap-2 justify-center items-center cursor-pointer">
                                 <p>{ t("no") }</p>
-                                <div className={`${!isStudentWantToReceiveNotification ? "bg-primary" : "bg-white border-2"} w-7 h-7 flex items-center justify-center rounded-md transition-all duration-150 ease-in-out`}>
+                                <div className={`${!isStudentWantToReceiveNotification ? "bg-primary" : "bg-white border-2"} w-6 h-6 flex items-center justify-center rounded-md transition-all duration-150 ease-in-out`}>
                                     <img src={check} alt="check" />
                                 </div>
                             </div>
