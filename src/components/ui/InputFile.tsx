1de32e0d0ffb6daebae14835ad9d75df8c8ffde2
FOKO KENMOGNE Wilfried <fokowilfried7@gmail.com>
2025-05-28 10:04:51 +0100

    update: start the oimplementation of the step 2 of the student register stepper

--- src/components/ui/InputFile.tsx
+++ src/components/ui/InputFile.tsx
@@ -1,28 +1,23 @@
-import folderIcon from "/icons/folder.svg";
-
 function InputFile({
   placeholder,
   isRequired,
   file,
-  label,
   handleChange,
   showValidationErrors,
   accept,
 }: {
   placeholder: string;
   isRequired: boolean;
-  file: File | undefined;
-  label: string;
-  handleChange: (file: File | undefined) => void;
+  file: File | null;
+  handleChange: (file: File | null) => void;
   showValidationErrors?: boolean;
   accept?: string;
 }) {
   return (
     <div className="flex flex-col gap-1">
-      <div>{ label }</div>
       <label className="relative cursor-pointer border border-gray-400 rounded-md h-10 flex items-center px-4 justify-between hover:bg-blue-50">
         <span className="text-gray-600">{file?.name || placeholder}</span>
-        <img src={folderIcon} alt="file" />
+        <img src="/icons" alt="file" />
         <input
           type="file"
           accept={accept}
