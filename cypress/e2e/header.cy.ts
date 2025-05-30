688f82d94cc10eb4216f41e3b08b1d9b2ec1cfac
FOKO KENMOGNE Wilfried <fokowilfried7@gmail.com>
2025-05-30 09:44:25 +0100

    update the cy test

--- cypress/e2e/header.cy.ts
+++ cypress/e2e/header.cy.ts
@@ -2,17 +2,21 @@ describe('header component', () => {
   it('test header only if exists', () => {
 
     cy.visit('/')
+    if(cy.url().should("include", "/dashboard")) {
+      //
+    } else {
+      cy.contains("Connectez vous").click();
+      // Should be on a new URL whitch include /login
+      cy.url().should("include", "/login");
+  
+      cy.contains("Sourcing").click();
+      cy.url().should("eq", Cypress.config().baseUrl + "/");
+  
+      cy.contains("Rejoignez nous !").click();
+      // Should be on a new URL whitch include /registration
+      cy.url().should("include", "/registration");
+    }
 
-    cy.contains("Connectez vous").click();
-    // Should be on a new URL whitch include /login
-    cy.url().should("include", "/login");
-
-    cy.contains("Sourcing").click();
-    cy.url().should("eq", Cypress.config().baseUrl + "/");
-
-    cy.contains("Rejoignez nous !").click();
-    // Should be on a new URL whitch include /registration
-    cy.url().should("include", "/registration");
 
   });
 });
