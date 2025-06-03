fe5ae814808824e1ce1289178864e160d666ca0d
FOKO KENMOGNE Wilfried <fokowilfried7@gmail.com>
2025-06-03 12:18:07 +0100

    update the navbar design

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,22 +8,25 @@ function NavBar() {
   const { t } = useTranslation();
   const { accountRole } = useAuth();
 
+  const [isExpanded, setIsExpanded] = useState(true); // <- Nouveau
+  const toggleNavbar = () => setIsExpanded((prev) => !prev); // <- Toggle function
+
   const [menu, setMenu] = useState(
-    accountRole == "company" ?
-    [
-      { title: "dashboard.navbar.dashboard_nav", icon: "/icons/dashboard.svg", route: "/dashboard", isSelected: false },
-      { title: "dashboard.navbar.student_nav", icon: "/icons/datalist.svg", route: "/datalist", isSelected: false },
-      { title: "dashboard.navbar.chat_nav", icon: "/icons/chat.svg", route: "/chats", isSelected: false },
-      { title: "dashboard.navbar.profile_nav", icon: "/icons/profile.svg", route: "/profile", isSelected: false },
-      { title: "dashboard.navbar.settings_nav", icon: "/icons/setting.svg", route: "/setting", isSelected: false },
-    ]
-    : [
-        { title: "dashboard.navbar.dashboard_nav", icon: "/icons/dashboard.svg", route: "/dashboard", isSelected: false },
-        { title: "dashboard.navbar.company_nav", icon: "/icons/datalist.svg", route: "/datalist", isSelected: false },
-        { title: "dashboard.navbar.profile_nav", icon: "/icons/profile.svg", route: "/profile", isSelected: false },
-        { title: "dashboard.navbar.settings_nav", icon: "/icons/setting.svg", route: "/setting", isSelected: false },
-    ]
-);
+    accountRole == "company"
+      ? [
+          { title: "dashboard.navbar.dashboard_nav", icon: "/icons/dashboard.svg", route: "/dashboard", isSelected: false },
+          { title: "dashboard.navbar.student_nav", icon: "/icons/datalist.svg", route: "/datalist", isSelected: false },
+          { title: "dashboard.navbar.chat_nav", icon: "/icons/chat.svg", route: "/chats", isSelected: false },
+          { title: "dashboard.navbar.profile_nav", icon: "/icons/profile.svg", route: "/profile", isSelected: false },
+          { title: "dashboard.navbar.settings_nav", icon: "/icons/setting.svg", route: "/setting", isSelected: false },
+        ]
+      : [
+          { title: "dashboard.navbar.dashboard_nav", icon: "/icons/dashboard.svg", route: "/dashboard", isSelected: false },
+          { title: "dashboard.navbar.company_nav", icon: "/icons/datalist.svg", route: "/datalist", isSelected: false },
+          { title: "dashboard.navbar.profile_nav", icon: "/icons/profile.svg", route: "/profile", isSelected: false },
+          { title: "dashboard.navbar.settings_nav", icon: "/icons/setting.svg", route: "/setting", isSelected: false },
+        ]
+  );
 
   useEffect(() => {
     setMenu((prevMenu) =>
@@ -35,46 +38,60 @@ function NavBar() {
   }, [currentPath]);
 
   return (
-    <div className="h-screen min-w-fit border-r-2 border-usual_light_purple">
-      <div className="!p-4">
-        <div className="w-fit h-screen flex flex-col gap-24">
-          <div className="flex items-center gap-4">
-            <div className="flex items-center">
-              <img src="/logo/sourcing_logo.svg" alt="logo" className="w-16" />
-              <p className="hidden md:block font-semibold text-primary">SOURCING</p>
+    <div
+      className={`h-screen ${
+        isExpanded ? "w-64" : "w-20"
+      } transition-all duration-300 relative ease-in-out border-r-2 border-usual_light_purple`}
+    >
+      <div className="p-4">
+        <div className="flex flex-col h-full gap-6 justify-between">
+          <div className="flex flex-col gap-6">
+            <div className="flex items-center gap-4">
+              <img src="/logo/sourcing_logo.svg" alt="logo" className="w-12" />
+              {isExpanded && <p className="font-semibold text-primary">SOURCING</p>}
+              {isExpanded && <p className="text-sm text-gray-500">v.01</p>}
             </div>
-            <p className="hidden md:block">v.01</p>
-          </div>
 
-          <div className="flex flex-col gap-2">
-            {menu.map((items, index) => (
-              <NavLink
-                key={index}
-                className={`flex gap-4 md:pl-2 items-center justify-center md:justify-start rounded-md h-12 cursor-pointer ${
-                  items.isSelected ? "bg-primary text-white" : ""
-                }`}
-                to={items.route}
-              >
-                <div
-                  className={`rounded-full w-8 h-8 flex items-center justify-center ${
-                    items.isSelected ? "text-white" : "text-black"
+            <div className="flex flex-col gap-2">
+              {menu.map((items, index) => (
+                <NavLink
+                  key={index}
+                  className={`flex gap-4 px-2 items-center rounded-md h-12 cursor-pointer transition-colors duration-150 ${
+                    items.isSelected ? "bg-primary text-white" : ""
                   }`}
+                  to={items.route}
                 >
                   <div
-                    className={`w-10 h-10 bg-cover bg-center transition-all duration-200 ease-in-out`}
-                    style={{
-                      backgroundColor: items.isSelected ? "white" : "#9197B3",
-                      maskImage: `url(${items.icon})`,
-                      WebkitMaskImage: `url(${items.icon})`,
-                      maskRepeat: "no-repeat",
-                      WebkitMaskRepeat: "no-repeat",
-                      maskPosition: "center",
-                    }}
-                  />
-                </div>
-                <p className="hidden md:inline">{t(items.title)}</p>
-              </NavLink>
-            ))}
+                    className={`rounded-full w-8 h-8 flex items-center justify-center ${
+                      items.isSelected ? "text-white" : "text-black"
+                    }`}
+                  >
+                    <div
+                      className={`w-10 h-10 bg-cover bg-center transition-all duration-200 ease-in-out`}
+                      style={{
+                        backgroundColor: items.isSelected ? "white" : "#9197B3",
+                        maskImage: `url(${items.icon})`,
+                        WebkitMaskImage: `url(${items.icon})`,
+                        maskRepeat: "no-repeat",
+                        WebkitMaskRepeat: "no-repeat",
+                        maskPosition: "center",
+                      }}
+                    />
+                  </div>
+                  {isExpanded && <p className="">{t(items.title)}</p>}
+                </NavLink>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex justify-center absolute -right-3 top-5.5">
+            <button
+              onClick={toggleNavbar}
+              className="w-6 h-6 flex items-center justify-center bg-usual_light_purple rounded-full hover:bg-primary hover:text-white transition-colors duration-200"
+              title="Toggle Navigation"
+            >
+              {isExpanded ? "<" : ">"}
+            </button>
           </div>
         </div>
       </div>
